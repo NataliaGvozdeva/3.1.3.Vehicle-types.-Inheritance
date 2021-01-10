d187058bee67ee062ca543006e745335fa57e0b5
Natalia Gvozdeva <gvozdeva-natalia@mail.ru>
2021-01-10 23:26:56 +0300

    Start creation of classes

---
+++ VehicleTypes/src/VehicleType.java
@@ -0,0 +1,16 @@
+class VehicleType {
+
+    protected String attribute;
+
+    public VehicleType(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getAttributeOfType() {
+        return attribute;
+    }
+    public String getTypeName() {
+        return "Some vehicle type name";
+    }
+
+}
---
+++ VehicleTypes/src/VehicleTypeEnum.java
@@ -0,0 +1,5 @@
+public enum VehicleTypeEnum {
+
+    TRUCK, CAR, PASSENGER, SEDAN, WAGON, PICKUP, BUS, PETROL, DIESEL, HYBRID, ELECTRIC
+
+}
